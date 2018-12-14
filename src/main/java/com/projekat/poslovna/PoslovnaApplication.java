cae7fe38ddc823b17c7db745567d95cfd019c1ef
DELL <aleksa.kolarski@yahoo.com>
2018-12-14 20:52:11 +0100

    Initial again

---
+++ src/main/java/com/projekat/poslovna/PoslovnaApplication.java
@@ -0,0 +1,14 @@
+package com.projekat.poslovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaApplication.class, args);
+	}
+
+}
+
